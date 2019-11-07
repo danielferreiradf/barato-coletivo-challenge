f2e0db28646422fd0bbc4e4b40510042494e443c
danielferreiradf <danielferreiradk@gmail.com>
2019-11-07 11:44:28 -0300

    setup header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,13 @@ import { createUseStyles } from "react-jss";
 
 import Cart from "../../assets/images/shopping-cart.svg";
 
-const Header = () => {
-  const classes = useStyles();
+const Header = ({ cartColor }) => {
+  const classes = useStyles({ cartColor });
 
   return (
-    <header className={classes.headerEl}>
+    <header className={classes.header}>
       <div className="container">
-        <div className={classes.header}>
+        <div className={classes.headerContent}>
           <h1 className={classes.title}>Barato Coletivo</h1>
           <img className={classes.cart} src={Cart} alt="Shopping Cart" />
         </div>
@@ -18,19 +18,22 @@ const Header = () => {
   );
 };
 
-const useStyles = createUseStyles(theme => ({
-  headerEl: {
-    backgroundColor: "blue"
-  },
+const useStyles = createUseStyles((theme, props) => ({
   header: {
+    backgroundColor: theme.color5,
+    borderBottom: `3px solid ${theme.color4}`
+  },
+  headerContent: {
     display: "flex",
     justifyContent: "space-between",
-    padding: "2rem 1rem"
+    padding: "1.5rem 1rem"
   },
   title: {
-    color: theme.color2
+    color: theme.color6,
+    fontSize: "2.5rem"
   },
   cart: {
+    background: props => props.cartColor,
     height: "3rem"
   }
 }));
--- src/styles/theme/theme.js
+++ src/styles/theme/theme.js
@@ -1,4 +1,8 @@
 export default {
-  color1: "green",
-  color2: "pink"
+  color1: "rgba(241, 237, 237, 1)",
+  color2: "rgba(229, 229, 229, 1)",
+  color3: "rgba(60, 60, 60, 1)",
+  color4: "rgba(21, 139, 218, 1)",
+  color5: "rgba(239, 180, 28, 1)",
+  color6: "#000"
 };
